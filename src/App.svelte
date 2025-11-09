4df49e6752d4445795f0aecc4862287527dda965
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-11-08 23:21:24 -0600

    feat: add Generator component and base structure
    - Created main Generator component
    - Set up initial application structure
    - Added base components (Slider, Checkbox, Button, etc.)

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Generator from "./lib/Generator.svelte";
+</script>
+
+<main>
+  <h1>Password Generator</h1>
+  <Generator />
+</main>
---
+++ src/lib/Generator.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Password from "./Password.svelte";
+  import Slider from "./Slider.svelte";
+  import Checkbox from "./Checkbox.svelte";
+  import Strength from "./Strength.svelte";
+  import Button from "./Button.svelte";
+</script>
+
+<div class="generator">
+  <Password />
+  <div class="generator__options">
+    <Slider />
+    <div class="checkboxes">
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+    </div>
+    <Strength />
+    <Button />
+  </div>
+</div>
